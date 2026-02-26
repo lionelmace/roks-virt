a379edc0d929d00065d15b2b146cfeec6ee0fba4
Lionel Mace <lionel.mace@gmail.com>
2026-02-26 15:57:19 +0100

    upload script cluster

---
+++ scripts/create-cluster.sh
@@ -0,0 +1,20 @@
+# Create a managed OpenShift cluster via CLI
+
+```sh
+ibmcloud is vpcc virt-vpc --resource-group-name virtualization
+```
+
+```sh
+ibmcloud ks cluster create vpc-gen2  \
+--name virt-roks  \
+--zone eu-de-2  \
+--vpc-id r010-0fe0b0ee-6a51-4858-819e-aa1eefe62611  \
+--subnet-id 02b7-0e77305a-a522-4fce-be4e-3b37dbafa36f  \
+--flavor cx2d.metal.96x192  \
+--workers 2  \
+--operating-system RHCOS  \
+--kube-version 4.20.14_openshift  \
+--disable-outbound-traffic-protection  \
+--cni OVNKubernetes  \
+--cos-instance crn:v1:bluemix:public:cloud-object-storage:global:a/ad09f476263c44cda2cdc697bd808a6f:cb0eb88b-2cfc-48e9-b833-26340218f938::
+```
--- terraform/testing.auto.tfvars
+++ terraform/testing.auto.tfvars
@@ -12,8 +12,6 @@ ocp_version                         = "4.19"
 disable_outbound_traffic_protection = true
 default_worker_pool_machine_type    = "bx2.4x16"
 # Bare Metal Profile, run command `ibmcloud is bm-prs`
-# BM Profile only available in Toronto MZR:
-# cx2d.metal.96x192, bx2d.metal.96x384, mx2d.metal.96x768
 
 # Skip zones if insufficient capacity within those zones
 excluded_zones = ["ca-tor-1", "ca-tor-3"]
